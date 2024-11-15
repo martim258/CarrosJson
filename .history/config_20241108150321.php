e250752e861d69a7cce495d312c30dd59742ec51
goncalo247 <alf@esmonserrate.org>
2024-11-15 14:41:28 +0000

    first commit

---
+++ .history/config_20241108150321.php
@@ -0,0 +1,16 @@
+<?php 
+
+define("_URL", 'https://turma12r.alunos.esmonserrate.org/martim/Carros/');
+
+define("_SERVER", 'localhost');
+define("_BDUSER", 'USERNAME');
+define("_BDPASS", 'PASSWORD');
+define("_BD", 'DBNAME');
+
+define("_CAMINHO_TEMPLATE", '../templates/base/');
+define("_CAMINHO_ADM", '../admin/');
+define("_CAMINHO_ERROS", "../templates/Errors/");
+
+define("_JSON_CARROS", "../json/dados.json");    
+
+?>
